6e4d425458bfef715cbf290b6230d959f2b9c6a7
Alexandr Lagornii <alexandrlagornii@gmail.com>
2023-12-10 01:24:20 +0200

    refactoring

--- scripts/multiplethreads_firefox.py
+++ scripts/multiplethreads_firefox.py
@@ -1,24 +1,20 @@
-from oigscanner.browser.templates import firefox_template
-from oigscanner.interface import interface
-from oigscanner.data.processing import normalize_data_oig
 import pandas as pd
 import sys
 
+from oigscanner.browser.templates import firefox_template
+from oigscanner.interface import interface
+from oigscanner.data.processing import normalize_data_oig
 
-try:
-    # Set paths
-    path_binary = "FirefoxPortable/App/Firefox64/firefox.exe"
-    path_driver = "geckodriver.exe"
 
-    # Get data
-    data_path = str(sys.argv[1])
-    data = pd.read_excel(data_path)
-    data = normalize_data_oig(data)
+# Set paths
+path_binary = "FirefoxPortable/App/Firefox64/firefox.exe"
+path_driver = "geckodriver.exe"
 
-    # Run scanner with given template
-    browser_template = firefox_template(path_binary, path_driver)
-    interface(browser_template, data, multiple_threads=True)
+# Get data
+data_path = str(sys.argv[1])
+data = pd.read_excel(data_path)
+data = normalize_data_oig(data)
 
-except Exception as e:
-    print(str(e))
-    sys.exit(1)
+# Run scanner with given template
+browser_template = firefox_template(path_binary, path_driver)
+interface(browser_template, data, multiple_threads=True)
--- scripts/onethread_firefox.py
+++ scripts/onethread_firefox.py
@@ -6,20 +6,15 @@ from oigscanner.data.processing import normalize_data_oig
 from oigscanner.interface import interface
 
 
-try:
-    # Set paths
-    path_binary = "FirefoxPortable/App/Firefox64/firefox.exe"
-    path_driver = "geckodriver.exe"
+# Set paths
+path_binary = "FirefoxPortable/App/Firefox64/firefox.exe"
+path_driver = "geckodriver.exe"
 
-    # Get data
-    data_path = str(sys.argv[1])
-    data = pd.read_excel(data_path)
-    data = normalize_data_oig(data)
+# Get data
+data_path = str(sys.argv[1])
+data = pd.read_excel(data_path)
+data = normalize_data_oig(data)
 
-    # Run scanner with given template
-    browser_template = firefox_template(path_binary, path_driver)
-    interface(browser_template, data)
-
-except Exception as e:
-    print(str(e))
-    sys.exit(1)
+# Run scanner with given template
+browser_template = firefox_template(path_binary, path_driver)
+interface(browser_template, data)
