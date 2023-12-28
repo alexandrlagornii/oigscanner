1e8d171016de02c7648a2dc295eda31e6a279517
Alexandr Lagornii <alexandrlagornii@gmail.com>
2023-12-28 21:48:26 +0200

    refactoring

---
+++ scripts/twothreads_firefox.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import sys
+import os
+
+from oigscanner.browser.templates import firefox_template
+from oigscanner.data.processing import normalize_data_oig
+from oigscanner.interface import interface
+
+
+# Set paths
+path_binary = "FirefoxPortable/App/Firefox64/firefox.exe"
+path_driver = "geckodriver.exe"
+
+# Get data
+data_path = str(sys.argv[1])
+data = pd.read_excel(data_path)
+data = normalize_data_oig(data)
+
+# Run scanner with given template
+browser_template = firefox_template(path_binary, path_driver)
+interface(browser_template, data, number_threads=2)
